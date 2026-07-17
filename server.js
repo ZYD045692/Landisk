01f74226399c6aef16c938f526578fecdae22f34
LanDisk <dev@landisk.local>
2026-07-17 18:42:22 +0800

    fix: 二维码换回 ANSI terminal (CLI 好看)，日志只记录 URL

--- server.js
+++ server.js
@@ -268,9 +268,10 @@ function startServer(portOverride) {
     // 生成二维码
     try {
       const qrcode = require('qrcode');
-      qrcode.toString(url, { type: 'utf8' }, (err, qr) => {
-        if (!err) logger.info(`📱 手机扫码访问:\n${qr}`);
+      qrcode.toString(url, { type: 'terminal', small: true }, (err, qr) => {
+        if (!err) process.stdout.write(qr + '\n');
       });
+      logger.info(`📱 手机扫码访问: ${url}`);
     } catch {
       logger.info(`📱 手机浏览器打开: ${url}`);
     }
